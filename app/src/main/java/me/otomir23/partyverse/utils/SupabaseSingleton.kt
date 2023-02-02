07e356bdfaadab23b156828244e8b32163b41b5b
Damir Modyarov <otomir@yandex.ru>
2023-02-02 22:10:02 +0300

    Create supabase singleton

---
+++ app/src/main/java/me/otomir23/partyverse/utils/SupabaseSingleton.kt
@@ -0,0 +1,27 @@
+package me.otomir23.partyverse.utils
+
+import android.content.Context
+import io.github.jan.supabase.SupabaseClient
+import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.gotrue.GoTrue
+import io.github.jan.supabase.postgrest.Postgrest
+import io.github.jan.supabase.realtime.Realtime
+import me.otomir23.partyverse.R
+
+object SupabaseSingleton {
+    private var client: SupabaseClient? = null
+
+    fun getClient(ctx: Context): SupabaseClient {
+        if (client == null) {
+            client = createSupabaseClient(
+                supabaseUrl = ctx.getString(R.string.supabase_url),
+                supabaseKey = ctx.getString(R.string.supabase_public_token)
+            ) {
+                install(GoTrue)
+                install(Realtime)
+                install(Postgrest)
+            }
+        }
+        return client!!
+    }
+}
