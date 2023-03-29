1d699a4c4ae78ac36861484e92292e3d59b4376d
Ayni9 <Renatkaveev@gmail.com>
2023-03-29 14:56:26 +0500

    changed splashscreen

--- app/src/main/java/co/kotekote/partyverse/ui/theme/Theme.kt
+++ app/src/main/java/co/kotekote/partyverse/ui/theme/Theme.kt
@@ -2,21 +2,31 @@ package co.kotekote.partyverse.ui.theme
 
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
+import androidx.compose.runtime.compositionLocalOf
 
-private val ColorPalette = darkColors(
+private val DarkColorPalette = darkColors(
     primary = Purple200,
     primaryVariant = Purple700,
     secondary = Teal200,
-    background = Color.Black,
-    surface = Color.Black,
+)
+
+private val LightColorPalette = lightColors(
+    primary = Purple500,
+    primaryVariant = Purple700,
+    secondary = Teal200,
 )
 
 @Composable
-fun PartyverseTheme(content: @Composable () -> Unit) {
+fun PartyverseTheme(darkTheme: Boolean, content: @Composable () -> Unit) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
     MaterialTheme(
-        colors = ColorPalette,
+        colors = colors,
         typography = Typography,
         shapes = Shapes,
         content = content
