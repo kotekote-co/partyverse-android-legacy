a81cb25f4320fc51ae1d82543acb49038e9038a8
Ayni9 <Renatkaveev@gmail.com>
2023-04-06 11:36:20 +0500

    Fix ExampleInstrumentedTest

--- app/src/androidTest/java/co/kotekote/partyverse/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/co/kotekote/partyverse/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("me.otomir23.partyverse", appContext.packageName)
+        assertEquals("co.kotekote.partyverse", appContext.packageName)
     }
 }
