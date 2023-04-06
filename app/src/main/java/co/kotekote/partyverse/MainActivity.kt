d0bacf80fdadb6f99fe004489e5b0196d0c480da
Ayni9 <Renatkaveev@gmail.com>
2023-04-06 12:11:43 +0500

    Add splashscreen via API

--- app/src/main/java/co/kotekote/partyverse/MainActivity.kt
+++ app/src/main/java/co/kotekote/partyverse/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import co.kotekote.partyverse.ui.navigation.SetupNavGraph
@@ -15,6 +16,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             PartyverseTheme(darkTheme = isSystemInDarkTheme()) {
                 navController = rememberNavController()
