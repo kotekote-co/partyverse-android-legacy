6a51681957d73d23ea10267f63f06aaac3eed914
Ayni9 <Renatkaveev@gmail.com>
2023-04-29 14:33:54 +0500

    Add edge-to-edge support

--- app/src/main/java/co/kotekote/partyverse/MainActivity.kt
+++ app/src/main/java/co/kotekote/partyverse/MainActivity.kt
@@ -4,14 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.Color
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
 import co.kotekote.partyverse.data.settings.subscribeToDarkTheme
 import co.kotekote.partyverse.data.settings.themePreferenceState
 import co.kotekote.partyverse.data.supabase.SupabaseSingleton
 import co.kotekote.partyverse.ui.navigation.PartyverseNavGraph
 import co.kotekote.partyverse.ui.theme.PartyverseTheme
 import com.google.accompanist.navigation.animation.rememberAnimatedNavController
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import io.github.jan.supabase.gotrue.handleDeeplinks
 
 class MainActivity : ComponentActivity() {
@@ -23,10 +27,20 @@ class MainActivity : ComponentActivity() {
         SupabaseSingleton.getClient(this).handleDeeplinks(intent)
         setContent {
             val preferredTheme by themePreferenceState(this)
-            PartyverseTheme(darkTheme = preferredTheme.subscribeToDarkTheme()) {
+            val isDarkTheme = preferredTheme.subscribeToDarkTheme()
+            PartyverseTheme(darkTheme = isDarkTheme) {
                 val navController = rememberAnimatedNavController()
                 PartyverseNavGraph(navController = navController)
+                val systemUiController = rememberSystemUiController()
+                SideEffect {
+                    systemUiController.setSystemBarsColor(Color.Transparent, !isDarkTheme)
+                }
             }
         }
+        extendScreen()
+    }
+
+    private fun extendScreen() {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
     }
 }
