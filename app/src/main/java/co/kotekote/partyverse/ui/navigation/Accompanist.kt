a9a365e2766844413caa2021b0bd7d61ba262928
Ayni9 <Renatkaveev@gmail.com>
2023-05-10 21:58:17 +0500

    Delete unused file

--- app/src/main/java/co/kotekote/partyverse/ui/navigation/Accompanist.kt
+++
@@ -1,7 +0,0 @@
-package co.kotekote.partyverse.ui.navigation
-
-object Accompanist {
-    private const val animationVersion = "0.31.0-alpha"
-    const val animations =
-        "com.google.accompanist:accompanist-navigation-animation:$animationVersion"
-}
